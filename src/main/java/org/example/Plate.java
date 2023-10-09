dac75c2bd41728401ed97d85a191726b69e00fc0
exphootzy <enjoyphoto@mail.ru>
2023-10-09 20:16:27 +0900

    create calss plate

--- src/main/java/org/example/Plate.java
+++ src/main/java/org/example/Plate.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public void addFood(int food) {
+        if (food < 30) {
+            System.out.println("\nFeed your cats!!!");
+        }
+        else System.out.println("\nFood is enough...");
+    }
 }
